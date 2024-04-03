8e2c6835804deb709eb2f147a49ff7ca32e4cae5
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-03 23:45:07 +0600

    added sign up and sign in page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
-import Home from "../pages/Home";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -9,7 +10,11 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
       },
     ],
   },
